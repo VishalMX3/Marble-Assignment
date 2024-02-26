69950038fcffa6fa73d35162b9e6b4823ed80753
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-26 16:36:18 +0530

    total orders from dummy data created

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+const data = {
+    data: {
+      data: [
+        { date: "2023-01-01", value: 100 },
+        { date: "2023-02-01", value: 150 },
+        { date: "2023-03-01", value: 200 },
+        { date: "2023-04-01", value: 180 },
+        { date: "2023-05-01", value: 250 },
+        { date: "2023-06-01", value: 120 },
+        { date: "2023-07-01", value: 280 },
+        { date: "2023-08-01", value: 90 },
+        { date: "2023-09-01", value: 220 },
+        { date: "2023-10-01", value: 170 },
+        { date: "2023-11-01", value: 200 },
+        { date: "2023-12-01", value: 160 },
+        { date: "2023-01-05", value: 130 },
+        { date: "2023-02-08", value: 190 },
+        { date: "2023-03-15", value: 210 },
+        { date: "2023-04-18", value: 140 },
+        { date: "2023-05-20", value: 270 },
+        { date: "2023-06-23", value: 230 },
+        { date: "2023-07-26", value: 110 },
+        { date: "2023-08-29", value: 180 },
+        { date: "2023-09-05", value: 250 },
+        { date: "2023-10-08", value: 120 },
+        { date: "2023-11-11", value: 300 },
+        { date: "2023-12-14", value: 170 },
+        { date: "2023-01-17", value: 190 },
+        { date: "2023-02-20", value: 220 },
+        { date: "2023-03-23", value: 130 },
+        { date: "2023-04-26", value: 240 },
+        { date: "2023-05-29", value: 160 },
+        { date: "2023-06-02", value: 190 },
+        { date: "2023-07-05", value: 210 },
+        { date: "2023-08-08", value: 140 },
+        { date: "2023-09-11", value: 170 },
+        { date: "2023-10-14", value: 200 }
+      ]
+    }
+  };
+  
+  export default data;
+  
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,18 +4,24 @@ import dayjs from "dayjs";
 import { ResponsiveAreaChart } from "../../components/dashboard/ResponsiveAreaChart";
 import { TabView } from "../../components/dashboard/TabView";
 import { IChartDatum, TTab } from "../../interfaces";
+import dummyTotalOrders from "../../data";
 
 const filters: CrudFilter[] = [
   {
     field: "start",
     operator: "eq",
-    value: dayjs()?.subtract(7, "days")?.startOf("day"),
+    value: dayjs().subtract(1, "year").startOf("month"),
   },
   {
     field: "end",
     operator: "eq",
-    value: dayjs().startOf("day"),
+    value: dayjs().endOf("month"),
   },
+  //   {
+  //     field: "step",
+  //     operator: "eq",
+  //     value: 2, // Step by two months
+  //   },
 ];
 
 export const Dashboard: React.FC = () => {
@@ -35,7 +41,6 @@ export const Dashboard: React.FC = () => {
         date: new Intl.DateTimeFormat("en-US", {
           month: "short",
           year: "numeric",
-          day: "numeric",
         }).format(new Date(item.date)),
         value: item?.value,
       }));
@@ -44,6 +49,7 @@ export const Dashboard: React.FC = () => {
 
   const memoizedRevenueData = useMemoizedChartData(dailyRevenue);
   const memoizedNewCustomersData = useMemoizedChartData(newCustomers);
+  const memoizedDummyTotalOrdersData = useMemoizedChartData(dummyTotalOrders);
 
   const tabs: TTab[] = [
     {
@@ -68,6 +74,21 @@ export const Dashboard: React.FC = () => {
         <ResponsiveAreaChart
           kpi="New customers"
           data={memoizedNewCustomersData}
+          colors={{
+            stroke: "rgb(76, 175, 80)",
+            fill: "rgba(54, 162, 235, 0.2)",
+          }}
+        />
+      ),
+    },
+
+    {
+      id: 3,
+      label: "Total Orders",
+      content: (
+        <ResponsiveAreaChart
+          kpi="Total Prders"
+          data={memoizedDummyTotalOrdersData}
           colors={{
             stroke: "rgb(76, 175, 80)",
             fill: "rgba(54, 162, 235, 0.2)",
