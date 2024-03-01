c11b11b99fbd0d492872a717c05e98a93f242e6f
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-01 11:07:46 +0530

    tabBar

--- src/components/dashboard/TabItem.tsx
+++ src/components/dashboard/TabItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IChartDatum2 } from "../../interfaces";
+import pen from "../../../images/pen.svg";
 
 type TTabItem = {
   label: string;
@@ -21,12 +22,12 @@ export const TabItem = ({ label, isActive, data, clickHandler }: TTabItem) => {
   const previousValueSum = calculateSumPrev(data);
 
   const calc = Math.round(
-    ((currentValueSum - previousValueSum) / previousValueSum) * 100
+    (Math.abs(currentValueSum - previousValueSum) / previousValueSum) * 100
   );
+
+  const sign = currentValueSum > previousValueSum ? "+" : "-";
   const percentDifference =
-    currentValueSum > previousValueSum ? `+ ${calc}%` : ` ${calc}%`;
-  const textColor =
-    currentValueSum > previousValueSum ? "text-green-500" : "text-red-500";
+    currentValueSum > previousValueSum ? `${calc}%` : `${calc}%`;
 
   return (
     <div
@@ -43,24 +44,31 @@ export const TabItem = ({ label, isActive, data, clickHandler }: TTabItem) => {
       >
         <div className="flex justify-between items-center gap-[25px] w-[163px] h-[23px]">
           <div className="m-auto w-[123px] relative h-[17px]">
-            <div className="flex flex-col items-start p-0 gap-[0px] absolute w-[123px] h-[16px] left-0 top-0">
-              <p className="w-[123px] h-[15px] font-medium text-xs text-[#303030]">
+            <div className="flex flex-col items-start p-0 gap-[0px] absolute h-[16px] left-0 top-0">
+              <p className="h-[15px] font-medium text-xs text-[#303030]">
                 {label}
               </p>
               <span className="w-[123px] h-[0px] border-dashed border-[1px] border-[#CCCCC]"></span>
             </div>
           </div>
-          <span className="flex flex-col justify-center items-center p-[5px] gap-2.5 m-auto w-[23px] h-[23px] rounded">
-            <p className="text-xs w-[13px] h-[13px] text">p</p>
+          <span className="hover:bg-[#c9cac7] ease-in flex flex-col justify-center items-center p-[5px] gap-2.5 m-auto w-[23px] h-[23px] rounded">
+            <img
+              src={pen}
+              alt="pen"
+              className=" text-xs w-[13px] h-[13px] text"
+            ></img>
           </span>
         </div>
 
-        <div className="flex items-center p-0 gap-[5px] w-[163px] h-[22px]">
-          <div className="w-[59px] h-[22px] text-[#303030] font-semibold text-base">
+        <div className="flex items-center p-0 w-[163px] h-[22px] gap-[5px]">
+          <div className="h-[22px] text-[#303030] font-semibold text-[15px]">
             {currentValueSum}
           </div>
-          <div className="w-[24px] h-[15px] gap-[2px] flex items-center ">
-            <span className="text-slate-500 w-[15px] h-[15px] font-normal text-xs">
+          <div className="w-[24px] h-[15px] gap-[2px] flex items-center p-0">
+            <span className="w-[7px] h-[10px] font-black text-[10px] flex items-center text-center text-[#616161]">
+              {sign}
+            </span>
+            <span className="text-slate-500 w-[15px] h-[15px] font-normal text-[10px]">
               {percentDifference}
             </span>
           </div>
