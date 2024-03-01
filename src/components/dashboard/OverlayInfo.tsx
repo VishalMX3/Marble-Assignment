34ecb6c97cf9c8313211a16d01c175fbd27d4f2f
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-01 21:11:39 +0530

    overlay outer div done

--- src/components/dashboard/OverlayInfo.tsx
+++ src/components/dashboard/OverlayInfo.tsx
@@ -1,6 +1,6 @@
 export const OverlayInfo: React.FC<{ info: string }> = ({ info }) => {
   return (
-    <div className="z-10 absolute top-[3px] left-[3px] p-2 bg-gray-200 rounded-md shadow-md">
+    <div className="ease-in flex flex-col z-10 absolute top-[1px] left-[2px] py-[15px] px-[10px] gap-2.5 bg-[#FFFFFF] rounded-[10px] shadow-md items-start w-[356px] h-[76px]">
       {info}
     </div>
   );
